d2685f054280a35eadc72952e26585d2e2434920
camenduru <54370274+camenduru@users.noreply.github.com>
2023-07-27 17:45:23 +0300

    test

--- dreambooth.py
+++ dreambooth.py
@@ -6,11 +6,11 @@ from gradio import strings
 
 class Dreambooth:
     def __init__(self):
-        self.pipe = None
+        pipe = None
 
     def run_live(command):
-      with os.popen(command) as self.pipe:
-        for line in self.pipe:
+      with os.popen(command) as pipe:
+        for line in pipe:
           line = line.rstrip()
           print(line)
           yield line
@@ -29,24 +29,27 @@ class Dreambooth:
 
     def test_text_to_image(output_dir, load_model, prompt, negative_prompt, num_inference_steps, guidance_scale):
         if load_model:
-            self.pipe = StableDiffusionPipeline.from_pretrained(output_dir, safety_checker=None, torch_dtype=torch.float16).to("cuda")
-            self.pipe.enable_xformers_memory_efficient_attention()
-        image = self.pipe(prompt, negative_prompt=negative_prompt, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images[0]
+            global pipe
+            pipe = StableDiffusionPipeline.from_pretrained(output_dir, safety_checker=None, torch_dtype=torch.float16).to("cuda")
+            pipe.enable_xformers_memory_efficient_attention()
+        image = pipe(prompt, negative_prompt=negative_prompt, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images[0]
         return image
 
     def test_dreambooth(output_dir, load_model, prompt, negative_prompt, num_inference_steps, guidance_scale):
         if load_model:
-            self.pipe = StableDiffusionPipeline.from_pretrained(output_dir, safety_checker=None, torch_dtype=torch.float16).to("cuda")
-            self.pipe.enable_xformers_memory_efficient_attention()
-        image = self.pipe(prompt, negative_prompt=negative_prompt, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images[0]
+            global pipe
+            pipe = StableDiffusionPipeline.from_pretrained(output_dir, safety_checker=None, torch_dtype=torch.float16).to("cuda")
+            pipe.enable_xformers_memory_efficient_attention()
+        image = pipe(prompt, negative_prompt=negative_prompt, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images[0]
         return image
 
     def test_lora(model_dir, load_model, output_dir, prompt, negative_prompt, num_inference_steps, guidance_scale):
         if load_model:
-            self.pipe = StableDiffusionPipeline.from_pretrained(model_dir, safety_checker=None, torch_dtype=torch.float16).to("cuda")
-            self.pipe.enable_xformers_memory_efficient_attention()
-            self.pipe.unet.load_attn_procs(output_dir)
-        image = self.pipe(prompt, negative_prompt=negative_prompt, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images[0]
+            global pipe
+            pipe = StableDiffusionPipeline.from_pretrained(model_dir, safety_checker=None, torch_dtype=torch.float16).to("cuda")
+            pipe.enable_xformers_memory_efficient_attention()
+            pipe.unet.load_attn_procs(output_dir)
+        image = pipe(prompt, negative_prompt=negative_prompt, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images[0]
         return image
 
     def clear_out_text():
