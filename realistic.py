966205d090b1881e5ffb2e7de3ae2ca48c97ab09
camenduru <54370274+camenduru@users.noreply.github.com>
2023-07-28 15:40:05 +0300

    test

--- realistic.py
+++ realistic.py
@@ -65,10 +65,10 @@ def launch():
     with trainer:
         with gr.Tab("Train"):
             with gr.Row():
-                with gr.Box():
+                with gr.Group():
                     files = gr.Files(label="Upload Images", file_types=["image"], file_count="multiple")
                     files.upload(fn=upload_file, inputs=files)
-                with gr.Box():
+                with gr.Group():
                     with gr.Accordion("Train Lora All Arguments", open=False):
                         gr.Markdown(
                         """
@@ -249,7 +249,7 @@ def launch():
                     update_command = gr.Button(value="Update train command")
                     btn_train_lora_run_live = gr.Button("Train Lora")
                     update_command.click(fn=update_instance_prompt, inputs=[learning_rate, max_train_steps, instance_prompt], outputs=lora_command)
-                    btn_train_lora_run_live.click(Shared.run_live, inputs=lora_command, outputs=train_lora_out_text, show_progress=True).then(set_textbox, None, train_lora_out_text, show_progress=True)
+                    btn_train_lora_run_live.click(Shared.run_live, inputs=lora_command, outputs=train_lora_out_text, show_progress=True).then(set_textbox, None, train_lora_out_text, show_progress=False)
         with gr.Tab("Test"):
             with gr.Row():
                 with gr.Box():
